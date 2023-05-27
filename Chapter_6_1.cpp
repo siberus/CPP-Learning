253c261f7677b459b58ff4880beed4c7efd37a5b
siberus <mr.siberus@yandex.ru>
2023-05-27 20:54:51 +0700

    Final task 6_1

--- Chapter_6_1.cpp
+++ Chapter_6_1.cpp
@@ -1,1 +1,32 @@
 #include <iostream>
+//#include <array>
+
+enum Items 
+    {
+        HEALTH_POISON,
+        TORCHS,
+        ARROWS,
+        MAX_ITEMS
+    };
+
+    int countTotalItems(int *arr, int length)
+    {
+        int count=0;
+        for (int i = 0; i < length; i++)
+        {
+            count +=arr[i];
+        }
+        
+
+        return count;
+    }
+
+int main ()
+{
+    int items[MAX_ITEMS] = {3, 6, 12};
+    std::cout << countTotalItems(items, MAX_ITEMS);
+
+    return 0;
+
+}
+
