3201f6ba4b20eef7ec53832848f91a95549ee845
siberus <mr.siberus@yandex.ru>
2023-05-29 21:09:47 +0700

    First commit task 6_6

--- Task_6_6.cpp
+++ Task_6_6.cpp
@@ -1,8 +1,43 @@
 #include <iostream>
 
-enum Range
+enum Levels
 {
-    3,
-    4,
+   CARD_2,
+   CARD_3,
+   CARD_4,
+   CARD_5,
+   CARD_6,
+   CARD_7,
+   CARD_8,
+   CARD_9,
+   CARD_10,
+   CARD_V,
+   CARD_D,
+   CARD_K,
+   CARD_T,
+   MAX_RANGE
+};
+
+enum Suits
+{
+    CLUBS,
+    DIAMONDS,
+    HEARTS,
+    SPADES,
+    MAX_SUITS   
+};
+
+struct Card
+{
+    int level;
+    int suit;
+};
+
+void printCard(const Card &p_card)
+{
+    char letterLevel, letterSuit;
+    
 
 }
+
+
