c50be48ea317444abed18353b4d5d8930a1f2e83
siberus <mr.siberus@yandex.ru>
2023-06-04 14:21:40 +0700

    Final ccommit for task_118_2

---
+++ Task 118_2.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <array>
+//#include <string>
+#include <string_view>
+#include <algorithm>
+//#include <functional> //Если не используем functions то не нужно
+
+struct Season 
+{ 
+    std::string_view name{}; 
+    double averageTemperature{};
+};
+
+int main()
+{
+    std::array<Season, 4> seasons
+    { 
+        { 
+            { "Spring", 285.0 }, 
+            { "Summer", 296.0 }, 
+            { "Fall", 288.0 }, 
+             { "Winter", 263.0 } 
+        } 
+    };
+   
+    
+    std::sort(seasons.begin(), seasons.end(),
+                [](const auto &a, const auto &b)
+                    {return (a.averageTemperature < b.averageTemperature);}); 
+    
+   
+    for (const auto& season : seasons)
+    { 
+        std::cout << season.name << '\n'; 
+    }
+    return 0;
+}
