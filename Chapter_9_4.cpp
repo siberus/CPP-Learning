d0f8999bd3ab92ccc97757c45d9eb465883955ef
siberus <mr.siberus@yandex.ru>
2023-06-16 20:42:05 +0700

    Final commit of chapter_9_4

--- Chapter_9_4.cpp
+++ Chapter_9_4.cpp
@@ -41,8 +41,12 @@ class FixedPoint
         return m_base + static_cast<double>(m_decimal) / 100; 
     }
 
+    friend bool operator==(const FixedPoint &fp1, const FixedPoint &fp2);
     friend std::ostream& operator<< (std::ostream& out, FixedPoint &a);
     friend std::istream& operator>> (std::ostream& out, FixedPoint &a);
+    friend FixedPoint operator+(const FixedPoint &fp1, const FixedPoint &fp2);
+
+    FixedPoint operator-() { return FixedPoint(-m_base, -m_decimal); }
 };
 
 std::ostream& operator<< (std::ostream& out, FixedPoint &fp)
@@ -61,39 +65,45 @@ std::istream& operator>> (std::istream& in, FixedPoint &fp)
     return in;
 }
 
+bool operator== (const FixedPoint &fp1, const FixedPoint &fp2)
+{
+    return (fp1.m_base == fp2.m_base && fp1.m_decimal == fp2.m_decimal);
+}
+FixedPoint operator+(const FixedPoint &fp1, const FixedPoint &fp2)
+{ 
+    return FixedPoint(static_cast<double>(fp1) + static_cast<double>(fp2)); 
+}
 
+void SomeTest() 
+{ 
+    std::cout << std::boolalpha;
+    std::cout << (FixedPoint(0.75) + FixedPoint(1.23) == FixedPoint(1.98)) << '\n'; 
+    // оба значения положительные, никакого переполнения
+    std::cout << (FixedPoint(0.75) + FixedPoint(1.50) == FixedPoint(2.25)) << '\n'; 
+    // оба значения положительные, переполнение
+    std::cout << (FixedPoint(-0.75) + FixedPoint(-1.23) == FixedPoint(-1.98)) << '\n'; 
+    // оба значения отрицательные, никакого переполнения
+    std::cout << (FixedPoint(-0.75) + FixedPoint(-1.50) == FixedPoint(-2.25)) << '\n'; 
+    // оба значения отрицательные, переполнение
+    std::cout << (FixedPoint(0.75) + FixedPoint(-1.23) == FixedPoint(-0.48)) << '\n'; 
+    // второе значение отрицательное, никакого переполнения
+    std::cout << (FixedPoint(0.75) + FixedPoint(-1.50) == FixedPoint(-0.75)) << '\n'; 
+    // второе значение отрицательное, возможно переполнение
+    std::cout << (FixedPoint(-0.75) + FixedPoint(1.23) == FixedPoint(0.48)) << '\n'; 
+    // первое значение отрицательное, никакого переполнения
+    std::cout << (FixedPoint(-0.75) + FixedPoint(1.50) == FixedPoint(0.75)) << '\n'; 
+    // первое значение отрицательное, возможно переполнение
+}
 
 int main() 
 { 
-/*     FixedPoint a(37, 58); 
+    SomeTest(); FixedPoint a(-0.48); 
     std::cout << a << '\n'; 
+    std::cout << -a << '\n'; 
+    std::cout << "Enter a number: "; // введите 5.678
+    std::cin >> a; 
+    std::cout << "You entered: " << a << '\n';
     
-     FixedPoint b(-3, 9); 
-     std::cout << b << '\n'; 
-     
-     FixedPoint c(4, -7); 
-     std::cout << c << '\n'; 
-     
-     FixedPoint d(-5, -7); 
-     std::cout << d << '\n'; 
-     
-     FixedPoint e(0, -3); 
-     std::cout << e << '\n'; 
-     
-     std::cout << static_cast<double>(e) << '\n';  */
-
-     FixedPoint a(0.03); 
-     std::cout << a << '\n'; 
-     
-     FixedPoint b(-0.03); 
-     std::cout << b << '\n'; 
-     
-     FixedPoint c(4.01); // сохранится, как 4.0099999..., поэтому нам нужно это всё округлить 
-     std::cout << c << '\n'; 
-     
-     FixedPoint d(-4.01);
-     std::cout << d << '\n';
-
-     return 0; 
+    return 0; 
      
 }
